eb7185dc3013b3831f3ce10cee2894fd3c9fa8aa
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2022-03-27 23:49:42 -0300

    feat: create RepositoryList component and change global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { RepositoryList } from "./components/RepositoryList";
+
 import "./styles/global.scss";
 
 export function App() {
-  return <h1>Hello, world!</h1>;
+  return <RepositoryList />;
 }
